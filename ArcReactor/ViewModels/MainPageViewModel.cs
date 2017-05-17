d64a88a96888b38c0cab7182509344b0eb1c30e2
Marcin Badurowicz <m@badurowicz.net>
2017-05-17 22:02:21 +0200

    refactor to proper MVVM

--- ArcReactor/ViewModels/MainPageViewModel.cs
+++ ArcReactor/ViewModels/MainPageViewModel.cs
@@ -5,10 +5,144 @@ using System.Threading.Tasks;
 using Template10.Mvvm;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+using Windows.Devices.Enumeration;
+using Windows.UI.Popups;
 
 namespace ArcReactor.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private BluetoothService bs;
+        public ObservableCollection<DeviceInformation> BluetoothSerialDevices { get; set; }
+
+        private DeviceInformation selectedDevice;
+
+        public DeviceInformation SelectedDevice
+        {
+            get
+            {
+                return selectedDevice;
+            }
+
+            set
+            {
+                if (this.selectedDevice != value)
+                {
+                    selectedDevice = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+
+        private bool isConnected;
+
+        public bool IsConnected
+        {
+            get
+            {
+                return isConnected;
+            }
+
+            set
+            {
+                if (this.isConnected != value)
+                {
+                    isConnected = value;
+                    ConnectButtonDescription = (isConnected) ? "Disconnect" : "Connect";
+
+                    RaisePropertyChanged(nameof(IsConnected));
+                }
+            }
+        }
+
+
+        DelegateCommand _connectCommand;
+        public DelegateCommand ConnectCommand
+            => _connectCommand ?? (_connectCommand = new DelegateCommand(async () =>
+            {
+                if (bs.IsConnected)
+                {
+                    bs.Disconnect();
+                }
+                else
+                {
+                    var result = await bs.ConnectAsync(SelectedDevice);
+                    if (!result)
+                    {
+                        await MessageBox("Connection failed");
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+
+                IsConnected = bs.IsConnected;
+            }, () => true));
+
+        DelegateCommand _sendPulseCommand;
+        public DelegateCommand SendPulseCommand
+            => _sendPulseCommand ?? (_sendPulseCommand = new DelegateCommand(async () =>
+            {
+                await bs.WriteAsync("pulse");
+            }, () => true));
+
+        DelegateCommand _sendStartupCommand;
+        public DelegateCommand SendStartupCommand
+            => _sendStartupCommand ?? (_sendStartupCommand = new DelegateCommand(async () =>
+            {
+                await bs.WriteAsync("startup");
+            }, () => true));
+
+        private async Task MessageBox(string text)
+        {
+            var m = new MessageDialog(text);
+            await m.ShowAsync();
+        }
+
+
+        private string connectButtonDescription = "Connect";
+
+        public string ConnectButtonDescription
+        {
+            get
+            {
+                return connectButtonDescription;
+            }
+
+            set
+            {
+                if (this.connectButtonDescription != value)
+                {
+                    connectButtonDescription = value;
+                    RaisePropertyChanged(nameof(ConnectButtonDescription));
+                }
+            }
+        }
+
+
+        public MainPageViewModel()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+                return;
+
+            bs = new BluetoothService();
+            PopulateDevicesList();
+        }
+
+        public async Task PopulateDevicesList()
+        {
+            BluetoothSerialDevices = new ObservableCollection<DeviceInformation>();
+
+            var pairedDevices = await bs.FindPairedDevicesAsync();
+            foreach (var item in pairedDevices)
+            {
+                BluetoothSerialDevices.Add(item);
+            }
+        }
+
+
     }
 }
--- ArcReactor/Views/MainPage.xaml.cs
+++ ArcReactor/Views/MainPage.xaml.cs
@@ -1,68 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
-using Windows.Devices.Enumeration;
-using Windows.UI.Popups;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace ArcReactor.Views
 {
     public sealed partial class MainPage : Page
     {
-        private BluetoothService bs;
-
         public MainPage()
         {
             InitializeComponent();
             NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
-
-            bs = new BluetoothService();
-            PopulateDevicesList();
-        }
-
-        public async Task PopulateDevicesList()
-        {
-            var pairedDevices = await bs.FindPairedDevicesAsync();
-            cbDevices.ItemsSource = pairedDevices;
-        }
-
-        private async void btnConnect_Click(object sender, RoutedEventArgs e)
-        {
-            if (bs.IsConnected)
-            {
-                bs.Disconnect();
-                btnConnect.Content = "Connect";
-                spBasic.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                var result = await bs.ConnectAsync(cbDevices.SelectedItem as DeviceInformation);
-                if (!result)
-                {
-                    await MessageBox("Connection Failed");
-                }
-                else
-                {
-                    btnConnect.Content = "Disconnect";
-                    spBasic.Visibility = Visibility.Visible;
-                }
-            }
-        }
-
-        private async Task MessageBox(string text)
-        {
-            var m = new MessageDialog(text);
-            await m.ShowAsync();
-        }
-
-        private async void SendPulse(object sender, RoutedEventArgs e)
-        {
-            await bs.WriteAsync("pulse");
-        }
-
-        private async void SendStartup(object sender, RoutedEventArgs e)
-        {
-            await bs.WriteAsync("startup");
         }
     }
 }
